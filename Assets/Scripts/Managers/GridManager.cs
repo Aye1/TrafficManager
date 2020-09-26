23698a2fce8b3a44c45d5b921847bbb8d34e6d62
Adrien Janvier <janvier.adrien@gmail.com>
2020-09-26 23:55:01 +0200

    Correct grid coordinates + hover debug

---
+++ Assets/Scripts/Managers/GridManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public static GridManager Instance { get; private set; }
+
+#pragma warning disable 0649
+    [SerializeField] private Vector2Int _gridSize;
+    [SerializeField] private Transform _groundTransform;
+#pragma warning disable 0649
+
+    private int _cellSize = 10;
+
+    private Dictionary<Vector2Int, GameObject> _objectsDict;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            _objectsDict = new Dictionary<Vector2Int, GameObject>();
+            SetGroundScale();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void SetGroundScale()
+    {
+        _groundTransform.localScale = new Vector3(_gridSize.x, 1.0f, _gridSize.y);
+    }
+
+    public void SetGridSize(Vector2Int newSize)
+    {
+        _gridSize = newSize;
+        SetGroundScale();
+    }
+
+    #region Position Management
+
+    public Vector3 GetSnappedPosition(Vector3 position)
+    {
+        Vector2Int gridPos = GetPositionOnGrid(position);
+        Vector3 newPos = new Vector3((gridPos.x + 0.5f) * _cellSize, transform.position.y, (gridPos.y + 0.5f) * _cellSize);
+        return newPos;
+    }
+
+    // TODO: Manage out of bounds
+    public Vector2Int GetPositionOnGrid(Vector3 position)
+    {
+        return new Vector2Int(Mathf.FloorToInt(position.x/_cellSize), Mathf.FloorToInt(position.z/_cellSize));
+    }
+
+    public bool IsPositionOnGrid(Vector2Int position)
+    {
+        return false;
+    }
+    #endregion
+
+    #region Manage Grid Elements
+    public bool CanAddElementAtPos(Vector2Int pos)
+    {
+        return !_objectsDict.ContainsKey(pos);
+    }
+
+    public void AddElementAtPos(Vector2Int pos, GameObject element)
+    {
+        if(CanAddElementAtPos(pos))
+        {
+            _objectsDict.Add(pos, element);
+        }
+    }
+
+    public GameObject GetElementAtPos(Vector2Int pos)
+    {
+        _objectsDict.TryGetValue(pos, out GameObject res);
+        return res;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Managers/MouseManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+enum ClickAction { None, PutDebugSphere };
+
+public class MouseManager : MonoBehaviour
+{
+    private ClickAction _currentAction;
+
+    public GameObject debugSphere;
+    public GameObject hoverGridCube;
+
+    public static MouseManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentAction = ClickAction.PutDebugSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageMouseHover();
+        ManageMouseClick();
+    }
+
+    private void ManageMouseHover()
+    {
+        switch(_currentAction)
+        {
+            case ClickAction.None:
+                break;
+            case ClickAction.PutDebugSphere:
+                ManageGridHover();
+                break;
+        }
+    }
+
+    private void ManageMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            switch(_currentAction)
+            {
+                case ClickAction.None:
+                    break;
+                case ClickAction.PutDebugSphere:
+                    PutDebugSphere();
+                    break;
+            }
+        }
+    }
+
+    private Vector3 GetWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+
+    private void PutDebugSphere()
+    {
+        Vector3 mousePos = GetWorldPosition();
+        Vector2Int gridPos = GridManager.Instance.GetPositionOnGrid(mousePos);
+        if (GridManager.Instance.CanAddElementAtPos(gridPos))
+        {
+            GameObject newObj = Instantiate(debugSphere, Vector3.zero, Quaternion.identity, transform);
+            newObj.transform.position = GridManager.Instance.GetSnappedPosition(mousePos);
+            GridManager.Instance.AddElementAtPos(gridPos, newObj);
+        }
+    }
+
+    private void ManageGridHover()
+    {
+        Vector3 mousePos = GetWorldPosition();
+        Vector3 snappedPos = GridManager.Instance.GetSnappedPosition(mousePos);
+        Debug.Log(mousePos + " - " + GridManager.Instance.GetPositionOnGrid(snappedPos));
+        hoverGridCube.transform.position = snappedPos;
+    }
+}
